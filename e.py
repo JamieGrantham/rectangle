a21536a0b0af9b801cc889a092a679fec9b86bfc
JamieGrantham <113033674+JamieGrantham@users.noreply.github.com>
2022-10-06 10:45:09 -0400

    Add files via upload

---
+++ e.py
@@ -0,0 +1,9 @@
+L = int(input("what is the length of the rectangle"))
+W = int(input("What is the width of the rectangle"))
+Guess = int(input("what is the area of this rectangle"))
+A = L * W
+
+if Guess == A:
+  print("Good job, that is correct.")
+else:
+    print("Incorrect, the answer was" + str(a))
